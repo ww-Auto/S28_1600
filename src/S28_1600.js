dfe0b2793ca5ada434427889480d1fa32c6427dc
ww-Auto <96040657+ww-Auto@users.noreply.github.com>
2023-04-20 16:57:49 +0900

    1600 update
    1.lib 생성해서 메인함수 줄이기
    2.1depth아닌 source는 row만 남기고 넘기도록

--- src/S28_1600.js
+++ src/S28_1600.js
@@ -1,6 +1,9 @@
 import playwright from "playwright"; 
 import fs from "fs"; 
 import xlsx from "xlsx";
+import * as clickEvent from '../lib/clickEvent.js';
+import * as dataParsing from '../lib/dataParsing.js';
+import * as rowSetting from '../lib/rowSetting.js';
 test(); 
 async function test(site){ 
 
@@ -10,6 +13,7 @@ async function test(site){
     mkdir(savepath); 
 
     var brokenInfo = new Array();
+    var sourceInfo = await dataParsing.getSourceList(path + "list_mode_export.xlsx"); 
 
     const map = {
         Num : "num", 
@@ -24,19 +28,19 @@ async function test(site){
     };
     brokenInfo.push(map); 
 
-    parsingData(path + site+'_client_error_(4xx)_inlinks.xlsx', brokenInfo);
+    dataParsing.parsingData(path + site+'_client_error_(4xx)_inlinks.xlsx', brokenInfo);                                         
 
     var i;
     var findResult = false;
-  
-
+    var one_depth = true;
+    const browser = await playwright.chromium.launch({headless: false, args: ['--start-maximized']}); 
     for(i=0;i<brokenInfo.length;i++){
         var xPath = brokenInfo[i].xpath
-        const browser = await playwright.chromium.launch({headless: false, args: ['--start-maximized']}); 
         const context = await browser.newContext({viewport: null});    
 
             const page = await context.newPage();
-            const sourceurl = brokenInfo[i].source
+   
+            const sourceurl = brokenInfo[i].source 
             var type = brokenInfo[i].type; 
             var destination = brokenInfo[i].destination; // 바로 쓸수 없고 다시 선언해야함 Why? 확인 필요  
             var alttext = brokenInfo[i].alttext; 
@@ -47,95 +51,91 @@ async function test(site){
             var num = brokenInfo[i].num; 
         
             // await page.waitForTimeout(1000);
-           try{
-          
-               await page.goto(sourceurl).catch(error => { 
-                console.log("not go to url", error); 
-                });
-                
-
-            // console.log(brokenInfo[i].source)
-            // await page.waitForTimeout(1000);         
-            // 쿠키 
-            await page.locator('#truste-consent-button').click().catch(error => {
-                console.log("not exist cookie"); 
-            });
-            // await page.waitForTimeout(1000);
-            await page.locator('//*[@id="ins-editable-button-1580496494"]').click().catch(error => {
-                console.log("not exist cookie"); 
-            });
-            // await page.waitForTimeout(1000);
-            await page.locator('//*[@id="header"]/div[2]/div/div/div[2]/a').click().catch(error => {
-                console.log("not exist cookie"); 
-            });
-           
-            const findXpath = await page.locator(xPath); 
-            if(findXpath != null || !findXpath.isEmpty() || findXpath != [] || findXpath != "[]"){   
-            // console.log(findXpath)
-            findResult = await clickItem(findXpath); 
-            // console.log(findResult+"findResult")
-            }
-            if(findResult == true){
-                await page.waitForTimeout(1000);
-                const response = await page.goto(destination, { timeout: 10000 }).catch(e => { });
+            if(one_depth == true) {
+                var filter_url = sourceInfo.filter(function(input){
+                    return input.Address == sourceurl; 
+                    }) 
                 try{
-                    
-                    status = response.status(); 
-                    setRow(num, type, sourceurl, destination, alttext, anchor, xPath, linkposition,findResult );
-                    brokenInfo[i].findResult ="true";
-                    
-                }catch(e){
-                    status = -1; 
-                }
-                // console.log(response, destination); 
-            }else{
-                if(destination.includes('p6')) {
-                    setRow(num, type, sourceurl, destination, alttext, status, anchor, xPath, linkposition, 
-                        "p6-qa");
-                    brokenInfo[i].findResult = "p6-qa";
-               
-                }
-                if(type=='HTML Canonical'){
-                    setRow(num, type, sourceurl, destination, alttext, status, anchor, xPath, linkposition, 
-                        "Canonical");
+                    if(destination.includes('p6')) { //destination이p6인 경우
+                        rowSetting.setRow(num, type, sourceurl, destination, alttext, status, anchor, xPath, linkposition, findResult);
+                        brokenInfo[i].findResult ="p6-qa";  
+                    }
+                    if(type=='HTML Canonical'){
+                        rowSetting.setRow(num, type, sourceurl, destination, alttext, status, anchor, xPath, linkposition, findResult);
                         brokenInfo[i].findResult = "Canonical";
-    
+        
+                    }
+                    if(filter_url == null || filter_url == ""){
+                        rowSetting.setRow(num, type, sourceurl, destination, alttext,  status, anchor, xPath, linkposition,findResult );
+                        brokenInfo[i].findResult ="N/A_Not source list";   
+                    }
+                }catch(e){
+                    console.log(e)
                 }
+             }
+        try{  
+           if(brokenInfo[i].findResult !="N/A_Not source list" ){
+                await page.goto(sourceurl).catch(error => { 
+                    console.log("not go to url", error); 
+                    });
                 
-                if(anchor == "" || anchor == null || anchor == 'undefined' || anchor.includes("{{")) {
-                    if(alttext == "" ||  alttext == null || alttext == 'undefined'){
-                        setRow(num, type, sourceurl, destination, alttext, status, anchor, xPath, linkposition, 
-                            "N/A_anchor is empty or invalid", "");
-                        console.log("anchor is empty or invalid"); 
+            // 쿠키 
+                await page.locator('#truste-consent-button').click().catch(error => {
+                    console.log("not exist cookie"); 
+                });
+                await page.locator('//*[@id="ins-editable-button-1580496494"]').click().catch(error => {
+                    console.log("not exist cookie"); 
+                });
+                await page.locator('//*[@id="header"]/div[2]/div/div/div[2]/a').click().catch(error => {
+                    console.log("not exist cookie"); 
+                });
+            //확장    
+                await clickEvent.expendAll(page);
+                const findXpath = await page.locator(xPath); 
+                if(findXpath != null || !findXpath.isEmpty() || findXpath != [] || findXpath != "[]"){   
+                findResult = await clickEvent.clickItem(findXpath); 
+                // console.log(findResult+"findResult")
+                }
+                if(findResult == true){
+                    await page.waitForTimeout(1000);
+                    const response = await page.goto(destination, { timeout: 10000 }).catch(e => { });
+                    try{
+                        status = response.status(); 
+                        rowSetting.setRow(num, type, sourceurl, destination, alttext,  status, anchor, xPath, linkposition,findResult );
+                        brokenInfo[i].findResult ="true";
+
+                    }catch(e){
+                        status = -1; 
+                    }
+                }else{
+                    if(anchor == "" || anchor == null || anchor == 'undefined' || anchor.includes("{{")) {
+                        if(alttext == "" ||  alttext == null || alttext == 'undefined'){
+                            rowSetting.setRow(num, type, sourceurl, destination, alttext, status, anchor, xPath, linkposition, findResult );
+                                brokenInfo[i].findResult ="N/A_anchor is empty or invalid";
+                        }
                     }
                 }
-            }
             
-            const row = {
-                num : num, 
-                type : type,
-                sourceurl : sourceurl,
-                destination : destination,
-                alttext : alttext, 
-                anchor : anchor, 
-                xpath : xPath, 
-                linkposition : linkposition,
-                findresult : findResult
-                }; 
-            console.log(row)
-          
-            // return map; 
-           
-           
+                const row = {
+                    num : num, 
+                    type : type,
+                    sourceurl : sourceurl,
+                    destination : destination,
+                    destination_status : status,
+                    alttext : alttext, 
+                    anchor : anchor, 
+                    xpath : xPath, 
+                    linkposition : linkposition,
+                    findresult : findResult
+                    }; 
+                console.log(row)
+           }
            }catch(e){
             console.log(e)
            }
            finally{
             await page.close()      
            }
-
-           
-        
     }
 
     fs.writeFileSync(savepath +site+ '_brokenlink_result.json',JSON.stringify(brokenInfo,null,2)); 
@@ -144,64 +144,9 @@ async function test(site){
 
     process.exit(0);
 }
-async function clickItem(item){
-    try{                               
-      
-        await item.click();  
-        
-        console.log(item + " 클릭성공"); 
-        return true;
-
-    }catch(e){
-        console.log(item + " 클릭실패", e); 
-        return false;     
-    }
-}
-
 function mkdir( dirPath ) {
     const isExists = fs.existsSync( dirPath );
     if( !isExists ) {
         fs.mkdirSync( dirPath, { recursive: true } );
     }
-}
-
-async function parsingData(filepath, brokenInfo){
-
-    var excelFile = xlsx.readFile(filepath);
-    var sheetName = excelFile.SheetNames[0];      
-    var firstSheet = excelFile.Sheets[sheetName];  
-    var jsonData = xlsx.utils.sheet_to_json( firstSheet, { defval : "" } );
-
-    jsonData.forEach((t, i) => {
-        const row = {
-            num : i,
-            type : t.Type,
-            source : t.Source,
-            destination : t.Destination,
-            alttext : t["Alt Text"], 
-            anchor : t.Anchor, 
-            xpath : t["Link Path"], 
-            linkposition : t["Link Position"],
-            findresult:t.findResult
-        }; 
-        brokenInfo.push(row); 
-    });   
-    brokenInfo.shift(); //첫번째 열 삭제
-}
-function setRow(num, type, sourceurl, destination, alttext, status, anchor, xpath, linkposition, findResult){
-
-    const row = {
-        num : num, 
-        type : type,
-        source : sourceurl,
-        destination : destination,
-        alttext : alttext, 
-        destination_status : status,  
-        anchor : anchor, 
-        xpath : xpath, 
-        linkposition : linkposition,
-        findResult : findResult
-    }; 
-  
-    return row; 
 }
